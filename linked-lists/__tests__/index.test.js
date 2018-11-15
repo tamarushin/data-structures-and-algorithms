8e189f9056d38df5ff38a57ac3e27fd7daac233f
Tama <tamarushin@gmail.com>
2018-11-15 15:46:30 -0700

    added tests for reversing a linkedlist and for removing a node

--- linked-lists/__tests__/index.test.js
+++ linked-lists/__tests__/index.test.js
@@ -39,4 +39,41 @@ describe('testing LinkedList class', () => {
     expect(list.head.next.next.value).toEqual(2);
     expect(list.head.next.next.next.value).toEqual(1);
   });
+
+  it('reverse method should reverse a LinkedList', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.reverse();
+    expect(list.head.value).toEqual(4);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(2);
+    expect(list.head.next.next.next.value).toEqual(1);
+  });
+
+  it('remove(value) method should remove a node from the list', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.remove(2);
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(4);
+  });
+
+  it('remove(value) method should remove a node from te beginning of a list', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.remove(1);
+    expect(list.head.value).toEqual(2);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(4);
+  });
 });
